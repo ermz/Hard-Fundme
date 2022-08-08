6c4767c8e00b0f5575b82f5ed217bafa47a73cb9
ermz <eramirez4369@gmail.com>
2022-08-07 23:56:06 -0400

    Hardhat fund me project, with simple deploument

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,8 @@
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    // when going for localhost or hardhat network we want to usev a mock
+}
---
+++ hardhat.config.js
@@ -0,0 +1,63 @@
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+//  * @type import('hardhat/config').HardhatUserConfig
+//  */
+
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+// const KOVAN_RPC_URL =
+//     process.env.KOVAN_RPC_URL ||
+//     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+// const RINKEBY_RPC_URL =
+//     process.env.RINKEBY_RPC_URL ||
+//     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+// const PRIVATE_KEY =
+//     process.env.PRIVATE_KEY ||
+//     "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+// const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
+module.exports = {
+    solidity: "0.8.9",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            // gasPrice: 130000000000,
+        },
+        // kovan: {
+        //     url: KOVAN_RPC_URL,
+        //     accounts: [PRIVATE_KEY],
+        //     chainId: 42,
+        //     blockConfirmations: 6,
+        //     gas: 6000000,
+        // },
+        // rinkeby: {
+        //     url: RINKEBY_RPC_URL,
+        //     accounts: [PRIVATE_KEY],
+        //     chainId: 4,
+        //     blockConfirmations: 6,
+        // },
+    },
+    // etherscan: {
+    //     apiKey: ETHERSCAN_API_KEY,
+    // },
+    // gasReporter: {
+    //     enabled: true,
+    //     currency: "USD",
+    //     outputFile: "gas-report.txt",
+    //     noColors: true,
+    //     // coinmarketcap: COINMARKETCAP_API_KEY,
+    // },
+    namedAccounts: {
+        deployer: {
+          default: 0,
+        },
+        user: {
+          default: 1
+        }
+    },
+}
