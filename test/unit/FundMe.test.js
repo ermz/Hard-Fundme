cd02c61e136add0dddd1af1a6fa2d6764606c149
ermz <eramirez4369@gmail.com>
2022-08-10 07:46:36 -0400

    Added additional test for FundMe contract

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,9 +1,11 @@
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 const { deployments, ethers, getNamedAccounts } = require("hardhat")
 
 describe("FundMe", function() {
     let fundMe
     let deployer
+    let mockV3Aggregator
+    const sendValue = ethers.utils.parseEther("1")
     beforeEach(async function() {
         // const accounts = await ethers.getSigners()
         // const accountOne = account[0]
@@ -17,6 +19,22 @@ describe("FundMe", function() {
         it("sets the aggregator addresses correctly", async function () {
             const response = await fundMe.priceFeed()
             assert.equal(response, mockV3Aggregator.address)
+        })
+    })
+
+    describe("fund", async function () {
+        it("Fails if you don't send enough ETH", async function () {
+            await expect(fundMe.fund()).to.be.revertedWith("You need to spend more ETH!")
+        })
+        it("Should update the amount funded data structure", async function () {
+            await fundMe.fund({ value: sendValue })
+            const response = await fundMe.addressToAmountFunded(deployer)
+            assert.equal(response.toString(), sendValue.toString())
+        })
+        it("Adds funder to array of funders", async function () {
+            await fundMe.fund({value: sendValue})
+            const funder = await fundMe.funders(0)
+            assert.equal(deployer, funder)
         })
     })
 })
