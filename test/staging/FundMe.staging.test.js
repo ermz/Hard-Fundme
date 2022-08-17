bb244c2caa1bc026461b1e349b6b6a7f06c0e3a9
ermz <eramirez4369@gmail.com>
2022-08-17 07:59:46 -0400

    Simple funding script

--- test/staging/FundMe.staging.test.js
+++ test/staging/FundMe.staging.test.js
@@ -3,10 +3,10 @@ const { developmentChains } = require("../../helper-hardhat-config")
 const { assert } = require("chai")
 
 developmentChains.includes(network.name) ? describe.skip :
-describe("FundMe", async function () {
+describe("FundMe Staging Tests", async function () {
     let fundMe
     let deployer
-    const sendValue = ethers.utils.parseEther("1")
+    const sendValue = ethers.utils.parseEther("0.1")
     beforeEach(async function () {
         deployer = (await getNamedAccounts()).deployer
         fundMe = await ethers.getContract("FundMe", deployer)
